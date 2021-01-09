2efd08dafb5a3b5e20b85c193830215f723041af
Gabriel Araujo <gabriel@kenzie.com.br>
2021-01-09 11:51:41 -0300

    tests: FizzBuzz module and function build tests

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -7,22 +7,24 @@ defmodule Fizzbuzz do
 
   defp handleFileRead({:ok, result}) do
     # "1, 2, 3, 4"
-    result
-    |> String.split(",")
-    |> Enum.map(&convertAndEvaluateNumbers/1)
+    result =
+      result
+      |> String.split(",")
+      |> Enum.map(&convertAndEvaluateNumbers/1)
+
+    {:ok, result}
   end
 
-  defp handleFileRead({:error, reason}) do
-    "Error reading the file: #{reason}"
-  end
+  defp handleFileRead({:error, reason}), do: {:error, "Error reading the file: #{reason}"}
 
   defp convertAndEvaluateNumbers(number) do
-    number |> String.to_integer() |> evaluateNumbers()
+    number
+    |> String.to_integer()
+    |> evaluateNumbers()
   end
 
   defp evaluateNumbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: 'FizzBuzz'
   defp evaluateNumbers(number) when rem(number, 3) == 0, do: 'Fizz'
   defp evaluateNumbers(number) when rem(number, 5) == 0, do: 'Buzz'
-
   defp evaluateNumbers(number), do: number
 end
--- test/fizzbuzz_test.exs
+++ test/fizzbuzz_test.exs
@@ -1,8 +1,15 @@
 defmodule FizzbuzzTest do
   use ExUnit.Case
-  doctest Fizzbuzz
 
-  test "greets the world" do
-    assert Fizzbuzz.hello() == :world
+  describe "build/1" do
+    test "when a valid file is provided, returns the converted list" do
+      expectedResponse = {:ok, [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 'Buzz', 'FizzBuzz', 'Buzz']}
+      assert Fizzbuzz.build("numbers.txt") == expectedResponse
+    end
+
+    test "when a invalid file is provided, returns an error" do
+      expectedResponse = {:error, "Error reading the file: enoent"}
+      assert Fizzbuzz.build("invalid.txt") == expectedResponse
+    end
   end
 end
